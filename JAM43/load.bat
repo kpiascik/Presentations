f14247bd1461efb5ee2c2cea26958b6044af850f
Adam Stanley <astanley@rim.com>
2012-09-26 08:00:16 -0700

    added parameter validation

--- JAM43/load.bat
+++ JAM43/load.bat
@@ -1,6 +1,13 @@
 @echo off
-REM This script can be used to sideload more than one BAR file at the same time
-REM Dependencies: BlackBerry 10 WebWorks SDK
+REM This script can be used to sideload a BAR file
+REM Dependencies: BlackBerry 10 WebWorks SDK v 1.0.2.9
 SET tools=C:\Progra~1\Resear~1\BLACKB~2.9\dependencies\tools\bin
 
+IF "%1" == "" GOTO MISSINGPARAMS
+IF "%2" == "" GOTO MISSINGPARAMS
+IF "%3" == "" GOTO MISSINGPARAMS
+
 "%tools%\blackberry-deploy.bat" -package %1 -device %2 -installApp -password %3
+
+:MISSINGPARAMS
+ECHO Missing parameters.  Correct usage: load.bat package-name device-ip device-pwd
